315f510049af956d6b71ee7ac321faa762ed6c31
yaozhixing <yaozhixing797600@163.com>
2020-04-26 20:56:01 +0800

    多页面构建

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,23 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); // html解析
 const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // 清除 dist 目录
 
+const glob = require("glob")
+
+function getEntry() {
+  let entryObj = {}
+  glob.sync("./src/pages/**/*.js").forEach((item) => {
+    let entryArr = []
+    const start = item.indexOf("pages/") + 6; // 起点
+    const end = item.lastIndexOf("/"); // 终点，最后一个反斜杆
+    const name = item.substring(start, end)
+    entryArr.push(item)
+    entryObj[name] = entryArr
+  })
+  return entryObj
+  // { home: ['.src/pages/home/index.js'] }
+}
+
+
 module.exports = {
   mode: "development", // "production" | "development"
 
